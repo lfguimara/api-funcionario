854c2b47f828a9cb19e355ace478dd9345215350
lfguimara <lfguimara25@hotmail.com>
2026-07-03 18:23:20 -0300

    Atualizando testes de pipeline

--- test/funcionarios.test.js
+++ test/funcionarios.test.js
@@ -5,18 +5,18 @@ const app = require("../app")
 //Escrita de um teste
 describe("API de funcionarios", () => {
     
-    It("Deve cadastrar um funcionario", async () => {
+    it("Deve cadastrar um funcionario", async () => {
         const resposta = await request(app)
         .post("/funcionarios")
         .send({
             nome: "Maria",
             cargo: "Analista"
         })
-        expect(resposta.statusCode.toBe)(201)
+        expect(resposta.statusCode).toBe(201)
 
-        expect(resposta.body.nome.toBe)("Maria")
+        expect(resposta.body.nome).toBe("Maria")
 
-        expect(resposta.body.cargo.toBe)("Analista")
+        expect(resposta.body.cargo).toBe("Analista")
     })
 
 })
