4b624b84b3aedbb17e138a9c7593311ff7b37559
lfguimara <lfguimara25@hotmail.com>
2026-07-06 18:32:35 -0300

    Atualizando codigo

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const routes = require("./routes/funcionarioRoute")
+const sequelize = require("./config/database")
+
+
+const app = express()
+
+app.use(express.json())
+app.use(routes)
+
+sequelize.sync()
+
+
+module.exports = app
---
+++ src/routes/funcionarioRoute.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const router = express.Router()
+
+const controller = require("../controllers/funcionarioController")
+
+
+router.get("/funcionario",controller.listar)
+router.post("/funcionario",controller.cadastrar)
+
+module.exports = router
